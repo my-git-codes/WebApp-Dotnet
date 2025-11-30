ac31c7b6ecd0241c9f140dbe5fba43c3412a476d
my-git-codes <shivanidandu07@gmail.com>
2025-11-30 11:54:13 +0530

    Employee Web App Get and post Methods

---
+++ WebAppProject/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppProject.Data;
+
+namespace WebAppProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeeController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var employees = dbContext.Employees.ToList();
+            return Ok(employees);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetEmployeeById(int id)
+        {
+            var employee = dbContext.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+        [HttpPost]
+        public IActionResult AddEmployee(Models.Entities.EmployeeDto employeeDto)
+        {
+            var employee = new Models.Entities.Employee
+            {
+                FirstName = employeeDto.FirstName,
+                LastName = employeeDto.LastName,
+                Email = employeeDto.Email,
+                HireDate = employeeDto.HireDate
+            };
+            dbContext.Employees.Add(employee);
+            dbContext.SaveChanges();
+            return CreatedAtAction(nameof(GetAllEmployees), new { id = employee.Id }, employee);
+        }
+    }
+}
---
+++ WebAppProject/Models/Entities/EmployeeDto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Identity.Client;
+
+namespace WebAppProject.Models.Entities
+{
+    public class EmployeeDto
+    {
+        //public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime HireDate { get; set; }
+    }
+}
