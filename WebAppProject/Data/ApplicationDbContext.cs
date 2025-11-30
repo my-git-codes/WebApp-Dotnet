543fdd07030239a18afe54d115f5f142261fc1e0
my-git-codes <shivanidandu07@gmail.com>
2025-11-30 11:54:13 +0530

    Employee Web App Get and post Methods

---
+++ WebAppProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppProject.Models.Entities;
+
+namespace WebAppProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ WebAppProject/Models/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppProject.Models.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public DateTime HireDate { get; set; }
+    }
+}
